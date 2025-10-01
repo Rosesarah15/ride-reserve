15a88c68d834e176f858e19a9b67f1d1b107a2f8
Rosesarah15 <rsnantaba@gmail.com>
2025-10-01 14:27:44 +0300

    connected project to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:bus_booking/firebase_options.dart';
 import 'package:bus_booking/home/presentation/pages/bus_booking_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ApplicationEntry());
 }
 
